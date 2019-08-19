834f79e31730d4df6dc44e37f34325b04b6d6eaa
Leonardo617 <leonardo.lucas.2010@hotmail.com>
2019-08-19 11:14:26 -0300

    primeiro commit

---
+++ conecta.php
@@ -0,0 +1,24 @@
+<?php
+
+
+	$host = "localhost";
+	$usuario = "root";
+	$senha = "";
+	$banco = "etec";
+
+
+	$conexao = new MySQLi("$host", "$usuario", "$senha", "$banco");
+
+	if ($conexao->connect_error) {
+		echo "Erro de Conexão";
+	}
+	
+	// else{
+	// 	echo "Conexão bem sucedida";
+	// }
+
+
+
+
+
+?>
